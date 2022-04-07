3212b8521ff417bfcc4d7b5e88881138b8b88207
Muoso <russo_luca@outlook.com>
2022-04-07 19:06:56 +0200

    second commit

--- Geometria/Program.cs
+++ Geometria/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Geometria;
+
+Console.WriteLine("Please specify the length of the rectangle's base");
+int rectangleBase = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Please specify the length of the rectangle's height");
+int rectangleHeight = Convert.ToInt32(Console.ReadLine());
+
+Rettangolo MioRettangolo = new Rettangolo(rectangleBase, rectangleHeight, "MioRettangolo");
+
+Console.WriteLine($"L'area del rettangolo misura: {MioRettangolo.calcolaArea()} cm2");
+Console.WriteLine($"Il perimetro del rettangolo misura: {MioRettangolo.calcolaPerimetro()} cm");
+
+
+Rettangolo SecondoRettangolo = new Rettangolo(4, 7, "SecondoRettangolo");
+Rettangolo TerzoRettangolo = new Rettangolo(7, 4, "TerzoRettangolo");
+Rettangolo QuartoRettangolo = new Rettangolo(4, 2, "QuartoRettangolo");
+Rettangolo QuintoRettangolo = new Rettangolo(14, 17, "QuintoRettangolo");
+
+SecondoRettangolo.stampaRettangolo();
+TerzoRettangolo.stampaRettangolo();
+QuartoRettangolo.stampaRettangolo();
+QuintoRettangolo.stampaRettangolo();
--- Geometria/Rettangolo.cs
+++ Geometria/Rettangolo.cs
@@ -10,12 +10,14 @@ namespace Geometria
     {
         private int baseRettangolo;
         private int altezzaRettangolo;
+        private string nomeRettangolo;
 
 
-        public Rettangolo(int baseRettangolo, int altezzaRettangolo)
+        public Rettangolo(int baseRettangolo, int altezzaRettangolo, string nomeRettangolo)
         {
             this.baseRettangolo = baseRettangolo;
             this.altezzaRettangolo = altezzaRettangolo;
+            this.nomeRettangolo=nomeRettangolo;
         }
 
         public int calcolaArea()
@@ -27,6 +29,15 @@ namespace Geometria
         {
             int perimetro = (baseRettangolo * 2) + (altezzaRettangolo * 2);
             return perimetro;
+        }
+
+        public void stampaRettangolo()
+        {
+            Console.WriteLine($"--{nomeRettangolo}--");
+            Console.WriteLine($"base: {baseRettangolo} cm");
+            Console.WriteLine($"altezza: {altezzaRettangolo} cm");
+            Console.WriteLine($"perimetro: {calcolaPerimetro()} cm");
+            Console.WriteLine($"area: {calcolaArea()} cm2");
         }
 
 
