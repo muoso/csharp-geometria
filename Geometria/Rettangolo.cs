94a250c2b49e7e0636a28ab349dd25413492eebd
Muoso <russo_luca@outlook.com>
2022-04-07 17:56:07 +0200

    Add project files.

---
+++ Geometria/Rettangolo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometria
+{
+    public class Rettangolo
+    {
+        private int baseRettangolo;
+        private int altezzaRettangolo;
+
+
+        public Rettangolo(int baseRettangolo, int altezzaRettangolo)
+        {
+            this.baseRettangolo = baseRettangolo;
+            this.altezzaRettangolo = altezzaRettangolo;
+        }
+
+        public int calcolaArea()
+        {
+            return baseRettangolo * altezzaRettangolo;
+        }
+
+        public int calcolaPerimetro()
+        {
+            int perimetro = (baseRettangolo * 2) + (altezzaRettangolo * 2);
+            return perimetro;
+        }
+
+
+
+    }
+}
